357f59642e53cbc695a48103a0919d23523cd1d5
Nikolay Georgiev <master033@gmail.com>
2018-07-27 15:11:21 +0300

    Fix zsh post-install fix script

--- zsh-postinstall-fix.sh
+++ zsh-postinstall-fix.sh
@@ -1,8 +1,13 @@
 #/usr/bin/evn sh
 
 echo "Adding imports to .zshrc"
-cat ~/dotfiles/imports/rc >> .zshrc
+cat ~/dotfiles/imports/rc >> ~/.zshrc
 
 echo "Adding imports to .zprofile"
-cat ~/dotfiles/imports/profile >> .zprofile
+cat ~/dotfiles/imports/profile >> ~/.zprofile
+
+echo "Adding imports to .profile"
+cat ~/dotfiles/imports/profile >> ~/.profile
+
+echo "Done."
 
