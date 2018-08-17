96f23739e680dd5438158c4ac93b2fda460ca270
Nikolay Georgiev <master033@gmail.com>
2018-08-17 17:26:05 +0300

    Add alias.

--- aliases.sh
+++ aliases.sh
@@ -12,6 +12,7 @@ alias goto_idcamp_web="goto $FLABS_HOME/idcamp/idcamp-web"
 alias goto_popboardz_cms="goto $FLABS_HOME/popboardz/popboardz-cms"
 alias goto_popboardz_backend="goto $FLABS_HOME/popboardz/popboardz-backend"
 alias goto_larctest_backend="goto $FLABS_HOME/larctest/larctest-dev"
+alias goto_larctest_cms="goto $FLABS_HOME/larctest/larctest-cms-dev"
 
 #===========================================================
 # development helpers                                      #
